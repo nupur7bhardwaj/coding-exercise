2fa8b41c884d77630f1a59692aa7efc0fbf35051
nupur7bhardwaj <111491951+nupur7bhardwaj@users.noreply.github.com>
2022-10-27 15:20:45 +0300

    Craeting Bingo function using if else statement.

--- function.js
+++ function.js
@@ -2,4 +2,13 @@ function MyCode (){
     console.log("At the shortcut!!")
 }
 MyCode();
-
+let number = 5;
+function bingo(number){
+    if (number>5){
+        return "Bingo!";
+    }else{
+        return "Not Bingo!";
+    }
+}
+bingo(7);
+console.log(bingo(7));
